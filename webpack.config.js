15bf3985e39cedcfe0b06ab1c69ed7cbe7bfd7c9
Tarun Sharma <tarun.softengg@gmail.com>
2016-09-29 12:29:09 +0530

    webpack configuration modified for dev and prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,93 @@
-const path = require('path');
-const webpack = require('webpack');
+'use strict';
 
-module.exports = {
-    devtool: 'eval',
-    entry: [
-        './client/index',
-        'webpack-hot-middleware/client?reload=true'
-    ],
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const DEV = process.env.NODE_ENV !== 'production';
+
+const config = {
+    entry: ['./client/index.js'],
+    debug: DEV,
+    devtool: DEV ? 'source-map' : 'source-map',
+    target: 'web',
     output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js',
-        publicPath: '/static/'
+        path: __dirname + '/client',
+        publicPath: '/',
+        filename: 'bundle.js'
     },
-    eslint: {
-        configFile: './.eslintrc'
+    module: {
+        loaders: [{
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                loaders: ['babel']
+            }, {
+                test: /\.jpe?g$|\.gif$|\.png$|\.ico$/,
+                loader: 'url-loader?name=[path][name].[ext]&context=./src'
+            }, {
+                test: /\.html/,
+                loader: 'file?name=[name].[ext]'
+            }, {
+                test: /\.css$/,
+                // or ?sourceMap&modules&importLoaders=1!postcss-loader
+                loader: DEV ? 'style!css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader' : ExtractTextPlugin.extract('style', 'css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader')
+                    // 'style-loader!css-loader?modules&importLoaders=1!postcss-loader'
+            },
+            { test: /\.json/, loader: 'json' },
+            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?mimetype=application/vnd.ms-fontobject' },
+            { test: /\.woff(2)?(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
+            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
+            { test: /.svg(\?v=\d+\.\d+\.\d+)?$|.svg$/, loader: 'file?name=[path][name].[ext]&context=./src&mimetype=image/svg+xml' }
+        ]
     },
     plugins: [
-        new webpack.HotModuleReplacementPlugin()
-    ],
-    module: {
-        loaders: [
-            {
-      test: /\.jsx?$/,
-      exclude: /node_modules/,
-      loader: 'react-hot!babel'
-    },
-    {
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loaders: ['babel-loader', 'eslint-loader'],
-       include: path.join(__dirname, 'client')
-    }
-        ],
+        // Output our index.html and inject the script tag
+        new HtmlWebpackPlugin({
+            template: './client/index.html',
+            inject: 'body'
+        }),
+        // Without this, Webpack would output styles inside JS - we prefer a separate CSS file
+        new ExtractTextPlugin('styles.css'),
 
-    },
-    assets: {
-        publicPath: '/static/',
-        watchOptions: {
-            aggregateTimeout: 300,
-            poll: true
-        },
-        noInfo: true
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+        })
+    ],
+    postcss: () => {
+        return [
+            require('precss'),
+            require('postcss-advanced-variables')({
+                variables: require('./src/colors')
+            }),
+            require('autoprefixer')({ browsers: ['last 2 versions'] })
+        ];
     }
 };
+
+if (DEV) {
+    console.log('dev build adding webpack-hot-middleware for DEV environment');
+    config.entry.push('webpack-hot-middleware/client');
+
+    config.plugins.push(
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    );
+} else {
+    console.log('production build adding UglifyJsPlugin plugin');
+    // Minify JS for production
+    config.plugins.push(
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false,
+                unused: true,
+                dead_code: true
+            }
+        }),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('"production"')
+            }
+        })
+    );
+}
+
+module.exports = config;
