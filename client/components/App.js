a3e9c3a2b0168e2f5e1b5c4ff81524f87a7f07cf
Tarun Sharma <tarun.softengg@gmail.com>
2016-09-29 12:30:32 +0530

    test componennt running with react HMR

--- client/components/App.js
+++ client/components/App.js
@@ -10,7 +10,7 @@ export default class App extends Component {
 
   render() {
     return ( <div> 
-      <h1>Hello world</h1>
+      <h1>Hello world from Redux Hapi </h1>
       </div>
         );
     }
