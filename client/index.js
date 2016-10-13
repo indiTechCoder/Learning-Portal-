5dd6857378f31de5d2c10fff9fc5e91ef9a2ee39
Tarun Sharma <tarun.softengg@gmail.com>
2016-10-13 18:51:24 +0530

    pre and post handeler added for hapi routes

--- client/index.js
+++ client/index.js
@@ -9,4 +9,6 @@ const store = configureStore();
 
 
 ReactDOM.render( <Provider store = { store }>
-	<App/></Provider> , document.getElementById('root'));
+            <App /> 
+        </Provider> , document.getElementById('root'));
+
